a65e40159fd1b56e47e1593bd898fbc890305b5c
KishwarMaheen <34351287+KishwarMaheen@users.noreply.github.com>
2020-11-30 17:49:21 +0600

    Solved static image not loading.

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 LOGOUT_REDIRECT_URL = 'home'
